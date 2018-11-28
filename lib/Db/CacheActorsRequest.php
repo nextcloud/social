fbaa3dd2cdc589545b4caaadc58572d1dfba07c6
Maxence Lange <maxence@artificial-owl.com>
2018-11-28 14:52:31 -0100

    CACHE_TTL -> CACHING_TIMEOUT
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Db/CacheActorsRequest.php
+++ lib/Db/CacheActorsRequest.php
@@ -42,7 +42,7 @@ use OCP\IDBConnection;
 class CacheActorsRequest extends CacheActorsRequestBuilder {
 
 
-	const CACHE_TTL = 60 * 24; // 1d
+	const CACHING_TIMEOUT = 60 * 24; // 1d
 
 	/**
 	 * CacheActorsRequest constructor.
@@ -180,7 +180,7 @@ class CacheActorsRequest extends CacheActorsRequestBuilder {
 	public function getRemoteActorsToUpdate(): array {
 		$qb = $this->getCacheActorsSelectSql();
 		$this->limitToLocal($qb, false);
-		$this->limitToCreation($qb, self::CACHE_TTL);
+		$this->limitToCreation($qb, self::CACHING_TIMEOUT);
 
 		$update = [];
 		$cursor = $qb->execute();
