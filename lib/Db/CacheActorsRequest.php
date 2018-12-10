a9cc7909508e173e268c63d39a1bc20e0c8d59da
Maxence Lange <maxence@artificial-owl.com>
2018-12-10 09:17:04 -0100

    check size of source entry before insert in database

--- lib/Db/CacheActorsRequest.php
+++ lib/Db/CacheActorsRequest.php
@@ -66,6 +66,11 @@ class CacheActorsRequest extends CacheActorsRequestBuilder {
 	 * @return int
 	 */
 	public function save(Person $actor): int {
+		$source = $actor->getSource();
+		if (sizeof($source) >= CoreRequestBuilder::SOURCE_LENGTH) {
+			$source = 'too_big';
+		}
+
 		$qb = $this->getCacheActorsInsertSql();
 		$qb->setValue('id', $qb->createNamedParameter($actor->getId()))
 		   ->setValue('account', $qb->createNamedParameter($actor->getAccount()))
@@ -84,7 +89,7 @@ class CacheActorsRequest extends CacheActorsRequestBuilder {
 		   ->setValue('name', $qb->createNamedParameter($actor->getName()))
 		   ->setValue('summary', $qb->createNamedParameter($actor->getSummary()))
 		   ->setValue('public_key', $qb->createNamedParameter($actor->getPublicKey()))
-		   ->setValue('source', $qb->createNamedParameter($actor->getSource()))
+		   ->setValue('source', $qb->createNamedParameter($source))
 		   ->setValue('details', $qb->createNamedParameter(json_encode($actor->getDetails())))
 		   ->setValue(
 			   'creation',
