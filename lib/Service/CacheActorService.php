3356464707b017a7351f1592bdcc8497687eaf58
emersonkfuri <emersonkfuri@gmail.com>
2019-01-17 15:35:04 -0200

    Always use $account as local account (suppress @instance)
    Signed-off-by: emersonkfuri <emersonkfuri@gmail.com>

--- lib/Service/CacheActorService.php
+++ lib/Service/CacheActorService.php
@@ -166,6 +166,9 @@ class CacheActorService {
 	 * @throws CacheActorDoesNotExistException
 	 */
 	public function getFromLocalAccount(string $account): Person {
+		if (strrpos($account, '@')) {
+			$account = substr($account, 0, strrpos($account, '@'));
+		}
 		return $this->cacheActorsRequest->getFromLocalAccount($account);
 	}
 
