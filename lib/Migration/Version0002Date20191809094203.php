c22724c0bb3ca33963b2a669b2847898bcb7ec37
Maxence Lange <maxence@artificial-owl.com>
2019-01-18 14:43:34 -0100

    enlarge some field in db
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

---
+++ lib/Migration/Version0002Date20191809094203.php
@@ -0,0 +1,112 @@
+<?php
+declare(strict_types=1);
+
+
+/**
+ * Nextcloud - Social Support
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@artificial-owl.com>
+ * @copyright 2018, Maxence Lange <maxence@artificial-owl.com>
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+namespace OCA\Social\Migration;
+
+
+use Closure;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\DBAL\Types\Type;
+use OCP\DB\ISchemaWrapper;
+use OCP\IDBConnection;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+
+/**
+ * Class Version0001Date20181219000003
+ *
+ * @package OCA\Social\Migration
+ */
+class Version0002Date20191809094203 extends SimpleMigrationStep {
+
+
+	/** @var IDBConnection */
+	private $connection;
+
+
+	/**
+	 * @param IDBConnection $connection
+	 */
+	public function __construct(IDBConnection $connection) {
+		$this->connection = $connection;
+	}
+
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return ISchemaWrapper
+	 * @throws SchemaException
+	 * @throws DBALException
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options
+	): ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		// -> VARCHAR(4000)
+		foreach (Version0002Date20191809094201::$editToChar255 as $edit) {
+			list($tableName, $field) = $edit;
+
+			$table = $schema->getTable($tableName);
+			if ($table->hasColumn($field)) {
+				continue;
+			}
+
+			$table->addColumn($field, Type::STRING, ['notnull' => false, 'length' => 255]);
+		}
+
+		return $schema;
+	}
+
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+
+		foreach (Version0002Date20191809094201::$editToChar255 as $edit) {
+			list($table, $field) = $edit;
+
+			$qb = $this->connection->getQueryBuilder();
+			$qb->update($table)
+			   ->set($field, $field . '_copy')
+			   ->execute();
+		}
+	}
+
+}
+
