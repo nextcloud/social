2081e9b3a564dbca19e0d4151ba32e60978f52e3
Maxence Lange <maxence@artificial-owl.com>
2019-01-21 15:10:41 -0100

    Model and Interface: Announce
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

---
+++ lib/Interfaces/Object/AnnounceInterface.php
@@ -0,0 +1,120 @@
+<?php
+declare(strict_types=1);
+
+
+/**
+ * Nextcloud - Social Support
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@artificial-owl.com>
+ * @copyright 2018, Maxence Lange <maxence@artificial-owl.com>
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+namespace OCA\Social\Interfaces\Object;
+
+
+use OCA\Social\Exceptions\ItemNotFoundException;
+use OCA\Social\Interfaces\IActivityPubInterface;
+use OCA\Social\Model\ActivityPub\ACore;
+use OCA\Social\Model\ActivityPub\Object\Announce;
+use OCA\Social\Service\MiscService;
+
+
+class AnnounceInterface implements IActivityPubInterface {
+
+
+	/** @var MiscService */
+	private $miscService;
+
+
+	/**
+	 * AnnounceInterface constructor.
+	 *
+	 * @param MiscService $miscService
+	 */
+	public function __construct(MiscService $miscService) {
+		$this->miscService = $miscService;
+	}
+
+
+	/**
+	 * @param ACore $activity
+	 * @param ACore $item
+	 */
+	public function activity(Acore $activity, ACore $item) {
+	}
+
+
+	/**
+	 * @param ACore $item
+	 */
+	public function processIncomingRequest(ACore $item) {
+//		if (!$item->gotObject()) {
+//			return;
+//		}
+		$object = $item->getObjectId();
+		$this->miscService->log('___' . json_encode($object));
+//
+//		try {
+//			$service = AP::$activityPub->getInterfaceForItem($item->getObject());
+//			$service->activity($item, $object);
+//		} catch (ItemUnknownException $e) {
+//		}
+//
+	}
+
+
+	/**
+	 * @param ACore $item
+	 */
+	public function processResult(ACore $item) {
+	}
+
+
+	/**
+	 * @param string $id
+	 *
+	 * @return ACore
+	 * @throws ItemNotFoundException
+	 */
+	public function getItemById(string $id): ACore {
+		throw new ItemNotFoundException();
+	}
+
+	/**
+	 * @param ACore $item
+	 */
+	public function save(ACore $item) {
+		/** @var Announce $item */
+//		$this->cacheDocumentsRequest->save($item);
+	}
+
+
+	/**
+	 * @param ACore $item
+	 */
+	public function delete(ACore $item) {
+//		$this->cacheDocumentsRequest->delete($item);
+	}
+
+
+}
+
