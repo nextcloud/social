5cd9e99a753a610b459a7edaaf06072f0742ae33
Jonas Sulzer <jonas@violoncello.ch>
2019-03-12 17:21:58 +0100

    🐛 FIX: lint build
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- src/components/TimelineEntry.vue
+++ src/components/TimelineEntry.vue
@@ -26,7 +26,8 @@
 				<!-- eslint-disable-next-line vue/no-v-html -->
 				<div class="post-message" v-html="formatedMessage" />
 				<div v-click-outside="hidePopoverMenu" class="post-actions">
-					<a v-if="this.item.actor_info.account !== this.cloudId" v-tooltip.bottom="t('social', 'Boost')" class="icon-boost" @click.prevent="boost" />
+					<a v-if="this.item.actor_info.account !== this.cloudId" v-tooltip.bottom="t('social', 'Boost')" class="icon-boost"
+						@click.prevent="boost" />
 					<a v-tooltip.bottom="t('social', 'Reply')" class="icon-reply" @click.prevent="reply" />
 					<div v-if="popoverMenu.length > 0" v-tooltip.bottom="t('social', 'More actions')" class="post-actions-more">
 						<a class="icon-more" @click.prevent="togglePopoverMenu" />
