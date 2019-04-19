51c2df3e7b564c8610b91af3b4763556822ff99b
Maxence Lange <maxence@artificial-owl.com>
2019-04-19 17:04:02 -0100

    missing use
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Command/NoteBoost.php
+++ lib/Command/NoteBoost.php
@@ -43,6 +43,7 @@ use OCA\Social\Service\NoteService;
 use OCA\Social\Service\PostService;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
