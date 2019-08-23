2998ca23e1eb66de56856f11d9d3402c1c4081e8
Cyrille Bollu <cyrpub@bollu.be>
2019-08-23 13:58:19 +0200

    Configuration for phpunit testing
    Signed-off-by: Cyrille Bollu <cyrpub@bollu.be>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../../../tests/bootstrap.php';
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../../../lib/base.php';
 
 OC_App::loadApp('social');
 OC_Hook::clear();
