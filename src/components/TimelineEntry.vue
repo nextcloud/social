7a5e6c2e79d99984c70fa21ccdad84fa0169ef0a
Jonas Sulzer <jonas@violoncello.ch>
2019-09-16 18:13:42 +0200

    👌 IMPROVE: add action icons to notification timeline
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- src/components/TimelineEntry.vue
+++ src/components/TimelineEntry.vue
@@ -1,11 +1,14 @@
 <template>
-	<div class="timeline-entry">
-		<div v-if="item.type === 'SocialAppNotification'">
-			{{ actionSummary }}
+	<div class="timeline-entry" @click="getSinglePostTimeline">
+		<div v-if="item.type === 'SocialAppNotification'" class="notification-header">
+			<div class="notification-icon" :class="notificationIcon" />
+			<span class="notification-action">
+				{{ actionSummary }}
+			</span>
 		</div>
 		<div v-if="item.type === 'Announce'" class="boost">
 			<div class="container-icon-boost">
-				<span class="icon-boost" />
+				<span class="icon-boost icon-boosting" />
 			</div>
 			<router-link v-if="item.actor_info" :to="{ name: 'profile', params: { account: item.local ? item.actor_info.preferredUsername : item.actor_info.account }}">
 				<span v-tooltip.bottom="item.actor_info.account" class="post-author">
@@ -32,11 +35,13 @@
 
 <script>
 import TimelinePost from './TimelinePost.vue'
+import UserEntry from './UserEntry.vue'
 
 export default {
 	name: 'TimelineEntry',
 	components: {
-		TimelinePost
+		TimelinePost,
+		UserEntry
 	},
 	props: {
 		item: { type: Object, default: () => {} }
@@ -92,6 +97,18 @@ export default {
 			}
 
 			return summary
+		},
+		notificationIcon() {
+			switch (this.item.subtype) {
+			case 'Like':
+				return 'icon-favorite'
+			case 'Announce':
+				return 'icon-boost'
+			case 'Follow':
+				return 'icon-user'
+			default:
+				return ''
+			}
 		}
 	},
 	methods: {
@@ -110,18 +127,52 @@ export default {
 		}
 	}
 
+	.notification-header {
+		display: flex;
+		align-items: bottom;
+	}
+
+	.notification-action {
+		flex-grow: 1;
+		display: inline-block;
+		align-self: flex-end;
+	}
+
+	.notification-icon {
+		margin: 5px 10px 0 5px;
+		opacity: .5;
+		background-position: center;
+		background-size: contain;
+		overflow: hidden;
+		height: 20px;
+		min-width: 32px;
+		flex-shrink: 0;
+	}
+
+	.icon-boost {
+		display: inline-block;
+		vertical-align: middle;
+	}
+
+	.icon-favorite {
+		display: inline-block;
+		vertical-align: middle;
+	}
+
+	.icon-user {
+		display: inline-block;
+		vertical-align: middle;
+	}
+
 	.container-icon-boost {
 		display: inline-block;
 		padding-right: 6px;
 	}
 
-	.icon-boost {
-		display: inline-block;
+	.icon-boosting {
 		width: 38px;
 		height: 17px;
-		opacity: .5;
 		background-position: right center;
-		vertical-align: middle;
 	}
 
 	.boost {
