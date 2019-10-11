4e50a4e4883b33e40418b6328ca63a2a66afe604
Cyrille Bollu <cyrpub@bollu.be>
2019-10-11 15:53:57 +0200

    FEATURE: Adds support in the front-end for replying to posts on remote ActivityPub instances
    Signed-off-by: Cyrille Bollu <cyrpub@bollu.be>

--- src/views/TimelineSinglePost.vue
+++ src/views/TimelineSinglePost.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="social__wrapper">
+		<composer />
 		<timeline-entry :item="mainPost" />
-		<timeline-list :type="$route.params.type" />
+		<!-- Do not show replies when composing a reply to a remote post -->
+		<timeline-list v-if="$route.name==='single-post'" />
 	</div>
 </template>
 
@@ -19,13 +21,14 @@
 </style>
 
 <script>
-import Logger from '../logger'
-import TimelineEntry from './../components/TimelineEntry.vue'
-import TimelineList from './../components/TimelineList.vue'
+import Composer from '../components/Composer.vue'
+import TimelineEntry from '../components/TimelineEntry.vue'
+import TimelineList from '../components/TimelineList.vue'
 
 export default {
 	name: 'TimelineSinglePost',
 	components: {
+		Composer,
 		TimelineEntry,
 		TimelineList
 	},
@@ -38,14 +41,28 @@ export default {
 	},
 	computed: {
 	},
+	mounted: function() {
+		// Tell the Composer component to prepare a reply 
+		this.$nextTick(function() {
+			if (this.$route.name === 'reply-remote') {
+				this.$root.$emit('composer-reply', this.mainPost)
+			}
+		})
+	},
 	beforeMount: function() {
 
 		// Get data of post clicked on
 		if (typeof this.$route.params.id === 'undefined') {
-			Logger.debug('displaying the single post timeline for a non logged-in user')
+			// Displaying the single post timeline for a non logged-in user
+			// or in case of a redirection from a remote instance (eg: a reply to remote post)
 			this.mainPost = JSON.parse(document.getElementById('postData').dataset.server)
 		} else {
 			this.mainPost = this.$store.getters.getPostFromTimeline(this.$route.params.id)
+		}
+
+		// No need to set params for the TimelineList component has we don't show it when replying to a remote post
+		if (this.$route.name === 'reply-remote') {
+			return
 		}
 
 		// Set params for the TimelineList component
@@ -53,7 +70,7 @@ export default {
 			account: window.location.href.split('/')[window.location.href.split('/').length - 2].substr(1),
 			id: window.location.href,
 			localId: window.location.href.split('/')[window.location.href.split('/').length - 1],
-			type: 'single-post'
+			type: this.$route.name
 		}
 
 		this.$store.dispatch('changeTimelineType', {
