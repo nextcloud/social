1e378654dc2c23ade910d259bac9293a9c58ac62
Cyrille Bollu <cyrpub@bollu.be>
2019-10-25 13:35:38 +0200

    Adds doc to some mixins
    Signed-off-by: Cyrille Bollu <cyrpub@bollu.be>

--- src/mixins/accountMixins.js
+++ src/mixins/accountMixins.js
@@ -7,6 +7,8 @@
  *
  * @file provides global account related methods
  *
+ * @mixin
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
