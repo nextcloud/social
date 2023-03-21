2ba5c828dba2a86ff5b1517d4df086fb29d7574f
Maxence Lange <maxence@artificial-owl.com>
2023-03-21 10:07:32 -0100

    filter duplicate
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Db/SocialFiltersQueryBuilder.php
+++ lib/Db/SocialFiltersQueryBuilder.php
@@ -41,7 +41,6 @@ class SocialFiltersQueryBuilder extends SocialLimitsQueryBuilder {
 	 * @deprecated ?
 	 */
 	public function filterDuplicate() {
-		return;
 		if (!$this->hasViewer()) {
 			return;
 		}
@@ -51,11 +50,11 @@ class SocialFiltersQueryBuilder extends SocialLimitsQueryBuilder {
 
 		$expr = $this->expr();
 		$filter = $expr->orX();
-//		$filter->add($this->exprLimitToDBFieldInt('filter_duplicate', 0, 's'));
+		$filter->add($this->exprLimitToDBFieldInt('filter_duplicate', 0, 's'));
 
 		$follower = $expr->andX();
 		$follower->add($this->exprLimitToDBField('attributed_to_prim', $this->prim($viewer->getId()), false));
-		$follower->add($expr->isNull('fs.id_prim'));
+//		$follower->add($expr->isNull('fs.id_prim'));
 		$filter->add($follower);
 
 		$this->andWhere($filter);
