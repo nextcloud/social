5e4d3f0916004a10467ef4fd8cd5f2e2ca66f2e8
Andy Scherzinger <info@andy-scherzinger.de>
2024-09-08 15:46:22 +0200

    chore: Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- lib/Listeners/ProfileSectionListener.php
+++ lib/Listeners/ProfileSectionListener.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
-// SPDX-FileCopyrightText: 2022 Carl Schwan <carl@carlschwan.eu>
-// SPDX-License-Identifier: AGPL-3.0-or-later
+/**
+ * SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 
 namespace OCA\Social\Listeners;
 
